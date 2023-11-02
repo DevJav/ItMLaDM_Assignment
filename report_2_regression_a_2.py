333f4e4aa93928d225bfa50d39a47ad9dccfc123
Javier Moreno <javiermorenoprieto@gmail.com>
2023-11-02 19:27:37 +0100

    fixed linear regression

--- report_2_regression_a_2.py
+++ report_2_regression_a_2.py
@@ -7,6 +7,7 @@ from sklearn import model_selection
 from matplotlib.pylab import (figure, semilogx, loglog, xlabel, ylabel, legend, 
                            title, subplot, show, grid, plot)
 from toolbox_02450 import rlr_validate
+from scipy import stats
 
 font_size = 15
 plt.rcParams.update({'font.size': font_size})
@@ -15,17 +16,24 @@ plt.rcParams.update({'font.size': font_size})
 filename = "data_mean_10.csv"
 df = pd.read_csv(filename)
 raw_data = df.values
-cols = np.r_[1:8]
+cols = np.r_[3:11]
 X = raw_data[:, cols]
-y = raw_data[:, 8]
-attributeNames = np.asarray(df.columns[cols])
+y = raw_data[:, [2]]
+attributeNames = list(np.asarray(df.columns[cols]))
+N, M = X.shape
 print(attributeNames)
 
-N, M = X.shape
+
+X = stats.zscore(X)
+# add offset attribute for w_0
+X = np.concatenate((np.ones((X.shape[0], 1)), X), axis=1)
+attributeNames = [u'offset']+attributeNames
+M = M+1
+
 K = 10
 
 lambdas = np.power(10.,range(-5,8))
-lambdas = np.logspace(-5, 5, 50)
+lambdas = np.logspace(-1.5, 5, 50)
 
 opt_val_err, opt_lambda, mean_w_vs_lambda, train_err_vs_lambda, test_err_vs_lambda = rlr_validate(X, y, lambdas, K)
 
@@ -34,7 +42,7 @@ subplot(1,2,1)
 semilogx(lambdas, mean_w_vs_lambda.T[:,1:], '.-') # Don't plot the bias term
 xlabel('Regularization factor')
 ylabel('Mean Coefficient Values')
-legend(attributeNames, loc='best', fontsize="small")
+legend(attributeNames[1:], loc='best', fontsize="small")
 grid()
 
 subplot(1,2,2)
